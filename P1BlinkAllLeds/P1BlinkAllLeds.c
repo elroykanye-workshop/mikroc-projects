cb3000882d26ee9a658146fc517102d1961a5f35
Elroy Kanye <elroykanye@gmail.com>
2022-01-21 14:29:27 +0100

    complete impl of led blinking

--- P1BlinkAllLeds/P1BlinkAllLeds.c
+++ P1BlinkAllLeds/P1BlinkAllLeds.c
@@ -4,5 +4,15 @@
 
 // Main program
 void main() {
-
-}
+     // Initialisations
+     TRISA = 0x1F; // set all PORTA as input
+     TRISB = 0x00; // set all PORTB as output
+     
+     // definition of endless loop
+     do {
+        PORTB = 0b11111111;
+        Delay_ms(500);
+        PORTB = 0b00000000;
+        Delay_ms(500);
+     } while (1); // end of loop
+} // end of program

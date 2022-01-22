5096f441841dc892ff60eca647e3765e9908db06
Elroy Kanye <elroykanye@gmail.com>
2022-01-22 05:00:57 +0100

    impl main with blink on RB0 only

--- P3BlinkOnlyRB0/P3BlinkOnlyRB0.c
+++ P3BlinkOnlyRB0/P3BlinkOnlyRB0.c
@@ -1,3 +1,14 @@
-void main() {
+// Declaration of global variables
 
+// Declaration of functions
+
+// Main program
+void main() {
+     TRISA = 0x1f;
+     TRISB = 0b11111110;
+     
+     do {
+        PORTB = 0b11111111;
+        Delay_ms(50)
+     }
 }
